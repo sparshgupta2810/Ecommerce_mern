47854f77c668bf26db23745428a7d5c5515800c4
sparshgupta2810 <sparshgupta2810@gmail.com>
2024-07-10 14:07:46 +0530

    new update

--- mart/src/components/Header.js
+++ mart/src/components/Header.js
@@ -53,7 +53,7 @@ const Header = () => {
     }
   }
   return (
-    <header className='h-16 shadow-md bg-white fixed w-full z-40'>
+    <header className='h-16 shadow-md bg-slate-100 fixed w-full z-40'>
       <div className=' h-full container mx-auto flex items-center px-4 justify-between'>
             <div className=''>
                 <Link to={"/"}>
